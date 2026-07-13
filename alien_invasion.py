ee68d44cb2147f2693dce3342747895383b542ca
jakerothacker <jakerothacker@gmail.com>
2026-07-12 23:10:17 -0400

    added ablility for fire bullets from ship, limited amount, played sound, and removed after they go off screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from arsenal import Arsenal
 
 class AlienInvasion:
 
@@ -17,7 +18,11 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
-        self.ship = Ship(self)
+        pygame.mixer.init()
+        self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
+        self.laser_sound.set_volume(0.7)
+
+        self.ship = Ship(self, Arsenal(self))
 
 
     def run_game(self):
@@ -54,6 +59,11 @@ class AlienInvasion:
             self.running = False
             pygame.quit()
             sys.exit()
+        elif event.key == pygame.K_SPACE
+            if self.ship.fire():
+                self.laser_sound.play
+                self.laser_sound.fadeout(250)
+
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
--- ship.py
+++ ship.py
@@ -3,12 +3,12 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
-
+    from arsenal import Arsenal
 
 class Ship:
 
 
-    def __init__(self, game: 'AlienInvasion'):
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -23,7 +23,13 @@ class Ship:
         self.moving_left = False
         self.x = float(self.rect.x)
 
+        self.arsenal = arsenal
+
     def update(self):
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
         temp_speed = self.settings.ship_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
             self.x += temp_speed
@@ -33,6 +39,8 @@ class Ship:
         self.rect.x = self.x
 
     def draw(self):
+        self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
 
-    
+    def fire(self):
+        return self.arsenal.fire_bullet
