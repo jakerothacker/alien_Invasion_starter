ddc2903c6a9d25c14dc2a0ecd41b3b6da8f89e82
jakerothacker <jakerothacker@gmail.com>
2026-07-12 22:15:04 -0400

    created ship class in ship.py and had it drawn on screen in alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -16,6 +17,9 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
+        self.ship = Ship(self)
+
+
     def run_game(self):
         while self.running:
             for event in pygame.event.get():
@@ -23,9 +27,12 @@ class AlienInvasion:
                     self.running = False
                     pygame.quit()
                     sys.exit()
+
             self.screen.blit(self.bg, (0,0))
+            self.ship.draw()
             pygame.display.flip()
             self.clock.tick(self.settings.FPS)
+
 
 
 
