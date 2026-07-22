d9b3dbe73b1edb57a52d6f7164d565134540afe4
jakerothacker <jakerothacker@gmail.com>
2026-07-22 14:54:34 -0400

    P2  ship now restes when it collieds with an alien

--- ship.py
+++ ship.py
@@ -19,12 +19,16 @@ class Ship:
         self.image = pygame.transform.scale(self.image, (self.settings.ship_w,self.settings.ship_h))
 
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.boundaries.midbottom
+        self._center_ship()
         self.moving_right = False
         self.moving_left = False
-        self.x = float(self.rect.x)
+        
 
         self.arsenal = arsenal
+
+    def _center_ship(self):
+        self.rect.midbottom = self.boundaries.midbottom
+        self.x = float(self.rect.x)
 
     def update(self):
         """Updates movement of ship and arsenal
@@ -56,3 +60,9 @@ class Ship:
             Bool: True if a bullet can be fired, False if not
         """
         return self.arsenal.fire_bullet()
+
+    def check_collisions(self, other_group):
+        if pygame.sprite.spritecollideany(self,other_group):
+            self._center_ship()
+            return True
+        return False
